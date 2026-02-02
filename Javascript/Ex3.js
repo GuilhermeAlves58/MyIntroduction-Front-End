2dcdf3d0f7c0503c974dd55826bfb8453c1de3d1
perkles <guilhermealves5194@gmail.com>
2026-02-02 18:42:21 -0300

    fix

--- Javascript/Ex3.js
+++ Javascript/Ex3.js
@@ -1,4 +1,3 @@
 var myString = prompt("text whatever you want:");
 var remaingCharacters = 140 - myString.length;
-console.log("you have written " + myString.length + " and you have left " + remaingCharacters)
 alert("you have written " + myString.length + " and you have left " + remaingCharacters);
