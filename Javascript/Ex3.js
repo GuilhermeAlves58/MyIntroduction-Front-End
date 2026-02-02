5b5c1b80b795cdfcd43ef3c04a97f0db9602e097
perkles <guilhermealves5194@gmail.com>
2026-02-02 18:33:13 -0300

    Exercise 3

---
+++ Javascript/Ex3.js
@@ -0,0 +1,4 @@
+var myString = prompt("text whatever you want:");
+var remaingCharacters = 140 - myString.length;
+console.log("you have written " + myString.length + " and you have left " + remaingCharacters)
+alert("you have written " + myString.length + " and you have left " + remaingCharacters);
