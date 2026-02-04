3ea454bbbfdeb96776dfbcb470ece6c7c217cf87
perkles <guilhermealves5194@gmail.com>
2026-02-04 16:42:10 -0300

    Execise 6

---
+++ Javascript/Ex6.js
@@ -0,0 +1,9 @@
+var dogAge = 2;
+var convertToHumanAge = ((dogAge - 2) * 4) + 21;
+console.log(convertToHumanAge); 
+
+
+/* 
+var dogAge = prompt("How old is your dog?");
+var convertToHumanAge = ((dogAge - 2) * 4) + 21;
+console.log("Your dog is " + convertToHumanAge + " in human years"); */ 
