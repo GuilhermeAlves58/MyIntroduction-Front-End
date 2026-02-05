4e701801fcba8526a51b1eca3b9e7d523614fc7c
perkles <guilhermealves5194@gmail.com>
2026-02-05 16:17:24 -0300

    Exercise 7

---
+++ Javascript/Ex7.js
@@ -0,0 +1,16 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    var turnToWeeks = ( 90 - age) * 52;
+    
+    var turnToDays = (90 - age)* 365;
+    
+    var remaingMonths = (90 * 12) - (age * 12);
+    
+    console.log("You have "+ turnToDays + " days, "+ turnToWeeks + " weeks, and "+ remaingMonths +" months left.")
+    
+/*************Don't change the code below**********/
+}
+
