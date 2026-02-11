3eb00b9536672302ba3bea796a907cc19ede8464
perkles <guilhermealves5194@gmail.com>
2026-02-11 15:17:53 -0300

    Javascript Intermediate Ex1

---
+++ Intermediate Javascript/Ex1.js
@@ -0,0 +1,4 @@
+prompt("Tell your name: ");
+prompt("Tell your partner's name: ");  
+var loveCalculator = Math.random();
+console.log("Relationship percentage " + Math.round(loveCalculator * 100)+ "%");
