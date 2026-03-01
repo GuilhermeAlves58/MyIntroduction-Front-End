d342956702ca38c4780d1c0f250bf6490e5c1a18
perkles <guilhermealves5194@gmail.com>
2026-03-01 18:24:00 -0300

    Advanced Dom&Javascript

--- Advanced Dom&Javascript/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Advanced Dom&Javascript/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,13 +1,41 @@
 var drumsNumbers = document.querySelectorAll(".drum").length;
 for (let index = 0; index < drumsNumbers; index++) {
-    document.querySelectorAll("button")[index].addEventListener("click", handleClick)    
+    document.querySelectorAll(".drum")[index].addEventListener("click", handleClick)
 }
 function handleClick(){
-    
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
-
     this.style.color = "white";
+    var buttonHTML = this.innerHTML;
+    switch (buttonHTML) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("sounds/kick.mp3");
+            kick.play();
+            break;
+        default: console.log(buttonHTML)
+    }
 }
 
 function add(num1, num2){
