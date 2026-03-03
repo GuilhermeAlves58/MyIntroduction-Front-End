50d38b8be8ce4cce6203f128de126ef14b4bf920
perkles <guilhermealves5194@gmail.com>
2026-03-03 13:07:20 -0300

    Advanced Dom&Javascript

--- Advanced Dom&Javascript/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Advanced Dom&Javascript/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,42 +1,60 @@
 var drumsNumbers = document.querySelectorAll(".drum").length;
+
 for (let index = 0; index < drumsNumbers; index++) {
-    document.querySelectorAll(".drum")[index].addEventListener("click", handleClick)
+
+    document.querySelectorAll(".drum")[index].addEventListener("click", function() {
+
+        var buttonHTML = this.innerHTML;
+        makeSound(buttonHTML);
+
+    });
+
 }
-function handleClick(){
-    this.style.color = "white";
-    var buttonHTML = this.innerHTML;
-    switch (buttonHTML) {
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+});
+
+
+
+function makeSound(key) {
+
+    switch (key) {
         case "w":
-            var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
+            new Audio("sounds/tom-1.mp3").play();
             break;
+
         case "a":
-            var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();
+            new Audio("sounds/tom-2.mp3").play();
             break;
+
         case "s":
-            var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
+            new Audio("sounds/tom-3.mp3").play();
             break;
+
         case "d":
-            var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
+            new Audio("sounds/tom-4.mp3").play();
             break;
+
         case "j":
-            var snare = new Audio("sounds/snare.mp3");
-            snare.play();
+            new Audio("sounds/snare.mp3").play();
             break;
+
         case "k":
-            var crash = new Audio("sounds/crash.mp3");
-            crash.play();
+            new Audio("sounds/crash.mp3").play();
             break;
+
         case "l":
-            var kick = new Audio("sounds/kick.mp3");
-            kick.play();
+            new Audio("sounds/kick.mp3").play();
             break;
-        default: console.log(buttonHTML)
+
+        default:
+            console.log(key);
     }
 }
+
+
+
+
 function HouseKeeper(yearsOfExperiences, name, cleaningRepertoire) {
     this.yearsOfExperiences = yearsOfExperiences;
     this.name = name;
