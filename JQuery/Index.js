dcf3b01b329a159afdbc8836cbdb26a291e9c43b
perkles <guilhermealves5194@gmail.com>
2026-03-06 16:10:12 -0300

    JQuery

--- JQuery/Index.js
+++ JQuery/Index.js
@@ -1,3 +1,35 @@
+// Comparação entre javascript e Jquery:Seletor de elementos
+$("h1").css("color","red");
 
- $("h1").css("color","red");
+// Comparação entre javascript e Jquery:manipulação de texto
+$("button").text("texto Do query selector");
+document.querySelectorAll("button").textContent = "texto";
 
+
+// Comparação entre javascript e Jquery:manipulação de atributos
+$("a").attr("href", "https://www.google.com/");
+document.querySelector("a").setAttribute("href","https://www.google.com/")
+
+// Comparação entre javascript e Jquery:manipulação de estilos
+$("h1").css("color", "blue");
+$("h1").css("font-size", "40px");
+document.querySelector("h1").style.color = "blue";
+document.querySelector("h1").style.fontSize = "40px";
+
+// Comparação entre javascript e Jquery: adição de event Listeners
+$("button").click(function(){
+    $("h1").css("color","purple");
+});
+for (let index = 0; index < 5; index++) {
+    document.querySelectorAll("button")[index].addEventListener("click",function(){
+        document.querySelector("h1").style.color = "purple";
+    })
+}
+
+$(document).keydown(function(event){
+    $("h1").text(event.key);
+})
+
+$("h1").on("mouseover", function(){
+    $("h1").css("color","purple");
+})
