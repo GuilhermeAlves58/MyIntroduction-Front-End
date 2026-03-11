65382db7ec1f683c861425912ebe50dab3c3913e
perkles <guilhermealves5194@gmail.com>
2026-03-11 16:34:21 -0300

    Boss Challenge 2

--- Boss Challenge 2/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Boss Challenge 2/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,6 +1,7 @@
-$
-
-let randonNumber =Math.floor(Math.random() * 3);
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
 function nextSequence(){
-
+    let randonNumber = Math.floor(Math.random() * 3);
+    let randomChosenColour = buttonColours[randonNumber];
+    gamePattern.push(randomChosenColour);
 }
