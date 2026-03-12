a00eea1571388b00293d3a09af1e6f96a6e78780
perkles <guilhermealves5194@gmail.com>
2026-03-12 17:32:38 -0300

    Step 3 - Show the Sequence to the User with Animations and Sounds

--- Boss Challenge 2/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Boss Challenge 2/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,7 +1,21 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+
 function nextSequence(){
-    let randonNumber = Math.floor(Math.random() * 3);
-    let randomChosenColour = buttonColours[randonNumber];
+
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+
     gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).addClass("flash");
+
+    setTimeout(function(){
+        $("#" + randomChosenColour).removeClass("flash");
+    }, 200);
+
+    let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
+
+nextSequence();  
