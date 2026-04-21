0e1fcd873c03b402161c1180b0bc6648620a79d6
perkles <guilhermealves5194@gmail.com>
2026-04-20 23:21:10 -0300

    Axios

--- APIs/5.3 Axios/solution.js
+++ APIs/5.3 Axios/solution.js
@@ -12,27 +12,34 @@ app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
-    res.render("index.ejs", { data: result });
+    console.log(result);
+    res.render("solution.ejs", { data: result });
   } catch (error) {
     console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
+    res.render("solution.ejs", {
       error: error.message,
     });
   }
 });
 
 app.post("/", async (req, res) => {
-  try{
+  try {
     console.log(req.body);
     const type = req.body.type;
     const participants = req.body.participants;
-    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`);
+    const response = await axios.get(
+      `https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`
+    );
     const result = response.data;
     console.log(result);
-    res.render("index.ejs", {data: result[Math.floor(Math.random() * result.length)]});
-  }catch (error){
-    console.error("failed to make request: ", error.message);
-    res.render("index.ejs",{error: "No activities that match your criteria."})
+    res.render("solution.ejs", {
+      data: result[Math.floor(Math.random() * result.length)],
+    });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("solution.ejs", {
+      error: "No activities that match your criteria.",
+    });
   }
 });
 
