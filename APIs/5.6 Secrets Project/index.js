e6a0f171531ef4af0ae393edec21b018845edeac
perkles <guilhermealves5194@gmail.com>
2026-04-24 18:24:15 -0300

    secrets project

--- APIs/5.6 Secrets Project/index.js
+++ APIs/5.6 Secrets Project/index.js
@@ -1,13 +1,18 @@
-// HINTS:
-// 1. Import express and axios
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
 
-// 2. Create an express app and set the port number.
+const port = 3000;
+const app = express();
 
-// 3. Use the public folder for static files.
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// 4. When the user goes to the home page it should render the index.ejs file.
+app.get("/", async (req, res) => {
+    const secrets = await axios.get("https://secrets-api.appbrewery.com/random");
+    res.render("index.ejs", {secret: secrets.data});
+})
 
-// 5. Use axios to get a random secret and pass it to index.ejs to display the
-// secret and the username of the secret.
-
-// 6. Listen on your predefined port and start the server.
+app.listen(port, () => {
+    console.log("Server running");
+})
