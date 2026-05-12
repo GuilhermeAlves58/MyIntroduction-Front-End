810cc288b4a1d5c44196afbbac31fdf5709650c8
perkles <guilhermealves5194@gmail.com>
2026-05-12 13:26:30 -0300

    insert values and edit values

--- PostgreSQL/8.6 Permalist Project/index.js
+++ PostgreSQL/8.6 Permalist Project/index.js
@@ -1,8 +1,20 @@
+import 'dotenv/config';
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
+const port = process.env.PORT;
 const app = express();
-const port = 3000;
+
+const db = new pg.Client({
+  host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,15 +31,22 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
+  const result = await db.query("INSERT INTO items (title) VALUES ($1)",[item]);
   items.push({ title: item });
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async (req, res) => {
+  const item = req.body.newItem;
+  const id = req.body.id;
+  const result = await db.query("UPDATE items SET title = ($1) WHERE id = ($2)", [item, id]);
+  
+  res.redirect("/");
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
