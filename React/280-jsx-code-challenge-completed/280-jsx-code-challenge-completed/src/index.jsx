07c7e6e54082af416d3862e64f7ab23e972b9f24
perkles <guilhermealves5194@gmail.com>
2026-07-23 17:12:19 -0300

    Javascript Expressions in JSX & ES6 Template Literals

---
+++ React/280-jsx-code-challenge-completed/280-jsx-code-challenge-completed/src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>paragrafo</h1>
+    <ul>
+      <li>vasco</li>
+      <li>flamego</li>
+      <li>fluminense</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
