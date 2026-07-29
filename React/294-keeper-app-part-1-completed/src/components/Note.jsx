ef1c38c683a35d0f2b5e8bf541db0df83b978a04
perkles <guilhermealves5194@gmail.com>
2026-07-29 17:10:26 -0300

    Keeper App Part 1 Solution

---
+++ React/294-keeper-app-part-1-completed/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Note title</h1>
+      <p>Note P</p>
+    </div>
+  );
+}
+
+export default Notes;
