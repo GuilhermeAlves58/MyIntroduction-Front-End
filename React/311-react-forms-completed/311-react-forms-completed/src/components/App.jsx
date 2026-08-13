2a5241abcad491032b94578f5ee884415a793741
perkles <guilhermealves5194@gmail.com>
2026-08-13 15:23:33 -0300

    .

--- React/311-react-forms-completed/311-react-forms-completed/src/components/App.jsx
+++ React/311-react-forms-completed/311-react-forms-completed/src/components/App.jsx
@@ -1,32 +1,28 @@
 import React, { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("");
-  const [headingText, setHeading] = useState("");
+  const [name, setName] = useState();
+  const [headingText, setheadingText] = useState();
 
   function handleChange(event) {
     console.log(event.target.value);
     setName(event.target.value);
   }
 
-  function handleClick(event) {
-    setHeading(name);
-
-    event.preventDefault();
+  function handleClick() {
+    setheadingText(name);
   }
-
   return (
     <div className="container">
-      <h1>Hello {headingText}</h1>
-      <form onSubmit={handleClick}>
-        <input
-          onChange={handleChange}
-          type="text"
-          placeholder="What's your name?"
-          value={name}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <h1>Hello {headingText} </h1>
+      <input
+        type="text"
+        placeholder="What's your name?"
+        onChange={handleChange}
+      />
+      <button type="submit" onClick={handleClick}>
+        Submit
+      </button>
     </div>
   );
 }
